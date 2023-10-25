132847fd9f0f93e7c64533bcec94b2ddecf2ad91
Alvin Philips <alvinphilips257@gmail.com>
2023-10-24 22:42:09 -0400

    Added Player load json code

--- Project1/Player.h
+++ Project1/Player.h
@@ -23,6 +23,26 @@ template <>
 inline void load_from_json(Player& value, const json::JSON& node)
 {
 	load_from_json((Object&)value, node);
+	
+	if (node.hasKey("speed")) {
+		load_from_json(value.speed, node.at("speed"));
+	}
+	if (node.hasKey("fire_delay")) {
+		load_from_json(value.fire_delay, node.at("fire_delay"));
+	}
+	if (node.hasKey("last_fired")) {
+		load_from_json(value.last_fired, node.at("last_fired"));
+	}
+	if (node.hasKey("movement_factor")) {
+		load_from_json(value.movement_factor, node.at("movement_factor"));
+	}
+	if (node.hasKey("velocity_dampener")) {
+		load_from_json(value.velocity_dampener, node.at("velocity_dampener"));
+	}
+	if (node.hasKey("lives")) {
+		load_from_json(value.lives, node.at("lives"));
+	}
+
 }
 
 #endif
